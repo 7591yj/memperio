a964b09bb3bf75d97bc58c1cf0d67a2b9d479259
7591yj <7591blitzloft@gmail.com>
2023-10-21 11:59:29 +0900

    FEAT ResultsScreen format

--- lib/src/learn/learn_widgets/results_screen.dart
+++ lib/src/learn/learn_widgets/results_screen.dart
@@ -1,25 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:memperio/src/widgets.dart';
 
-class ResultsScreen extends StatelessWidget {
+class ResultsScreen extends StatefulWidget {
   final List<dynamic> data;
 
   const ResultsScreen({super.key, required this.data});
 
   @override
+  State<ResultsScreen> createState() => _ResultsScreenState();
+}
+
+class _ResultsScreenState extends State<ResultsScreen> {
+  int solvedHowMuch = 0;
+  int correct = 0;
+  List<String> wrong = [];
+
+  void resultBuilder() {
+    for (var result in widget.data) {
+      if (result == true) {
+        correct++;
+      } else {
+        wrong.add(result as String);
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    solvedHowMuch = widget.data.length;
+    resultBuilder();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    print(data);
+    print(wrong);
     return Scaffold(
       appBar: AppBar(
         title: const Text('학습 완료'),
         automaticallyImplyLeading: false,
         centerTitle: true,
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('End of the list reached. Results:'),
-            // Display the final information or results here
+            const Header('결과'),
+            StyledCircularPercentIndicator(
+              text: '$solvedHowMuch / $correct',
+              percent: correct / solvedHowMuch,
+            ),
+            const Header('틀린 문제 다시보기')
           ],
         ),
       ),
